99449f9a1a7beb7ff95368cca50fa3383f0d8eac
Heromyth <bitworld@qq.com>
2018-07-23 11:11:11 +0800

    Create demo for SimpleHTTP

---
+++ examples/SimpleHTTP-Demo/source/client.d
@@ -0,0 +1,62 @@
+
+import hunt.http.client.http.SimpleHTTPClient;
+import hunt.http.client.http.SimpleResponse;
+
+import hunt.util.concurrent.Promise;
+import hunt.util.concurrent.CompletableFuture;
+
+import kiss.logger;
+
+import std.conv;
+import std.stdio;
+
+void main(string[] args) {
+
+	enum host = "127.0.0.1";
+	enum port = "3333";
+
+	SimpleHTTPClient client = new SimpleHTTPClient();
+
+	trace("step 1.....");
+	for (int i = 0; i < 1; i++) {
+		client.post("http://" ~ host ~ ":" ~ port ~ "/postData")
+				.put("RequestId", i.to!string ~ "_")
+				.bodyContent("test post data, hello foo " ~ i.to!string)
+				.submit( (SimpleResponse r) {
+					writeln(r.getStringBody());
+				});
+	}
+
+	// FIXME: Needing refactor or cleanup -@zxp at 7/23/2018, 10:40:57 AM
+	// 
+	// trace("step 2.....");
+	// for (int i = 10; i < 11; i++) {
+	// 	client.post("http://" ~ host ~ ":" ~ port ~ "/postData")
+	// 			.put("RequestId", i.to!string ~ "_")
+	// 			.bodyContent("test post data, hello foo " ~ i.to!string)
+	// 			.submit()
+	// 			.thenAccept( (SimpleResponse r) {
+	// 				writeln(r.getStringBody());
+	// 			});
+	// }
+
+	// trace("step 3.....");
+	// for (int i = 30; i < 31; i++) {
+	// 	CompletableFuture!SimpleResponse future = client
+	// 			.post("http://" ~ host ~ ":" ~ port ~ "/postData")
+	// 			.put("RequestId", i.to!string ~ "_")
+	// 			.bodyContent("test post data, hello foo " ~ i.to!string)
+	// 			.submit();
+				
+	// 	SimpleResponse r = future.get();
+	// 	if( r is null)
+	// 	{
+	// 		warning("no response");
+	// 	}
+	// 	else
+	// 		writeln(r.getStringBody());
+	// }
+
+	// getchar();
+	client.stop();
+}
---
+++ examples/SimpleHTTP-Demo/source/server.d
@@ -0,0 +1,71 @@
+
+import hunt.http.codec.http.model.HttpHeader;
+import hunt.http.codec.http.model.HttpStatus;
+import hunt.http.codec.http.model.MimeTypes;
+
+import hunt.http.server.http.SimpleHTTPServer;
+import hunt.http.server.http.SimpleRequest;
+import hunt.http.server.http.SimpleResponse;
+
+import hunt.container;
+import hunt.util.string;
+
+import kiss.logger;
+
+import std.conv;
+import std.datetime;
+import std.stdio;
+
+
+void main(string[] args)
+{
+	SimpleHTTPServer server = new SimpleHTTPServer();
+        server.headerComplete( (SimpleRequest req) {
+            List!(ByteBuffer) list = new ArrayList!ByteBuffer();
+            req.onContent( (ByteBuffer b) {
+				list.add(b);
+				})
+               .onContentComplete( (SimpleRequest request) {
+                   string msg = BufferUtils.toString(list);
+                   StringBuilder s = new StringBuilder();
+                   s.append("content complete").append("\r\n")
+                    .append(req.toString()).append("\r\n")
+                    .append(req.getFields().toString()).append("\r\n")
+                    .append(msg).append("\r\n");
+                   trace(s.toString());
+                   request.put("msg", msg);
+               })
+               .onMessageComplete( (SimpleRequest request) {
+                   SimpleResponse response = req.getResponse();
+                   string path = req.getRequest().getURI().getPath();
+
+                   trace("path=", path);
+
+                   response.getResponse().getFields().put(HttpHeader.CONTENT_TYPE, MimeTypes.Type.TEXT_PLAIN.asString());
+
+                   switch (path) {
+                       case "/":
+                           trace(request.getRequest().toString());
+                           trace(request.getRequest().getFields());
+                           string msg = BufferUtils.toString(list);
+                           trace(msg); // TODO:
+                           msg = "server demo 4. " ~ Clock.currTime.toISOExtString();
+						   response.write(msg);
+
+                        //    response.getResponse().getFields().put(HttpHeader.CONTENT_LENGTH, msg.length.to!string);
+
+                           break;
+                       case "/postData":
+                           response.write("receive message -> " ~ request.get("msg"));
+                           break;
+
+                       default:
+                           response.getResponse().setStatus(HttpStatus.NOT_FOUND_404);
+                           response.write("resource not found");
+                           break;
+                   }
+
+               });
+
+        }).listen("0.0.0.0", 3333);
+}
