8df3957a780f45cfaeee4767c61aec5a1844ee27
Heromyth <bitworld@qq.com>
2018-08-24 18:34:12 +0800

    Certificates for demo updated

--- examples/Http2ServerDemo/source/app.d
+++ examples/Http2ServerDemo/source/app.d
@@ -9,6 +9,15 @@ import std.file;
 import std.path;
 import std.stdio;
 
+
+/**
+openssl genrsa -out ca.key 4096
+openssl req -new -x509 -days 365 -key ca.key -out ca.crt
+openssl genrsa -out server.key 4096
+openssl req -new -key server.key -out server.csr
+openssl x509 -req -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
+*/
+
 void main(string[] args)
 {
 	string currentRootPath = dirName(thisExePath);
@@ -18,7 +27,7 @@ void main(string[] args)
     .useCertificateFile(currentRootPath ~ "/cert/server.crt", currentRootPath ~ "/cert/server.key")
     // httpServer() // For HTTP
     .router().get("/").handler((RoutingContext ctx) {
-        info("Request received.");
+        info("Resposing a HTTP request.");
         ctx.end("hello world!");
     }) //  .router().get("/static/*").handler(new StaticFileHandler(path.toAbsolutePath().toString()))
     .listen("0.0.0.0", 8081);
