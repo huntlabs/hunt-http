c0c0ceac2224310c272066bc25450ff3916ef8a8
heromyth <bitworld@qq.com>
2019-04-19 10:30:31 +0800

    Minor updates

--- examples/WebSocketDemo/source/client.d
+++ examples/WebSocketDemo/source/client.d
@@ -1,4 +1,3 @@
-
 import hunt.http.codec.http.model;
 
 import hunt.http.client.ClientHttpHandler;
@@ -15,6 +14,7 @@ import hunt.http.codec.websocket.stream.WebSocketPolicy;
 
 import hunt.concurrency.Promise;
 import hunt.concurrency.FuturePromise;
+import hunt.concurrency.CompletableFuture;
 
 import hunt.net.secure.SecureSessionFactory;
 import hunt.net.secure.conscrypt;
@@ -25,57 +25,63 @@ import hunt.logging;
 import std.datetime;
 import std.conv;
 import std.stdio;
+import hunt.net.NetUtil;
 
 /**
 sudo apt-get install libssl-dev
 */
-
 void main(string[] args) {
-	HttpClient client = new HttpClient(new HttpConfiguration());
-    HttpClientConnection connection = client.connect("127.0.0.1", 8080).get();
+
+    NetUtil.startEventLoop();
+
+    HttpClient client = new HttpClient(new HttpConfiguration());
+    Completable!(HttpClientConnection) conn = client.connect("127.0.0.1", 8080);
+
     HttpClientRequest request = new HttpClientRequest("GET", "/index");
     FuturePromise!WebSocketConnection promise = new FuturePromise!WebSocketConnection();
-    connection.upgradeWebSocket(request, WebSocketPolicy.newClientPolicy(), promise, 
-        
-        new class AbstractClientHttpHandler {
-            override
-            public bool messageComplete(HttpRequest request, HttpResponse response,
-                                           HttpOutputStream output,
-                                           HttpConnection connection) {
-                tracef("upgrade websocket success: " ~ response.toString());
-                return true;
-            }
-        }, 
-        
-        new class IncomingFrames {
-            override
-            public void incomingError(Exception t) {
+    IncomingFramesEx incomingFramesEx = new IncomingFramesEx();
+    ClientHttpHandlerEx handlerEx = new ClientHttpHandlerEx();
 
-            }
+    HttpClientConnection connection = conn.get();
+    assert(connection !is null);
 
-            override
-            public void incomingFrame(Frame frame) {
-                FrameType type = frame.getType();
-                switch (type) {
-                    case FrameType.TEXT: {
-                        TextFrame textFrame = cast(TextFrame) frame;
-                        tracef("Client received: " ~ textFrame.toString() ~ ", " ~ textFrame.getPayloadAsUTF8());
-                        break;
-                    }
-
-                    default: 
-                        warningf("Can't handle the frame of ", type);
-                        break;
-                }
-            }
-        }
-    );
+    connection.upgradeWebSocket(request, WebSocketPolicy.newClientPolicy(),
+            promise, handlerEx, incomingFramesEx);
 
     WebSocketConnection webSocketConnection = promise.get();
-    webSocketConnection.sendText("Hello WebSocket").thenAccept( (r) {
+    webSocketConnection.sendText("Hello WebSocket").thenAccept((r) {
         tracef("Client sends text frame success.");
     });
 
+    getchar();
     client.stop();
+}
 
+class ClientHttpHandlerEx : AbstractClientHttpHandler {
+    override public bool messageComplete(HttpRequest request,
+            HttpResponse response, HttpOutputStream output, HttpConnection connection) {
+        tracef("upgrade websocket success: " ~ response.toString());
+        return true;
+    }
+}
+
+class IncomingFramesEx : IncomingFrames {
+    override public void incomingError(Exception t) {
+
+    }
+
+    override public void incomingFrame(Frame frame) {
+        FrameType type = frame.getType();
+        switch (type) {
+        case FrameType.TEXT: {
+                TextFrame textFrame = cast(TextFrame) frame;
+                tracef("Client received: " ~ textFrame.toString() ~ ", " ~ textFrame.getPayloadAsUTF8());
+                break;
+            }
+
+        default:
+            warningf("Can't handle the frame of ", type);
+            break;
+        }
+    }
 }
