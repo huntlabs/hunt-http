3311c0c824e5c59f071ce51736cf81f8f9e8d699
heromyth <bitworld@qq.com>
2019-12-30 15:49:24 +0800

    Add get/set Attribute to HttpRequest

--- source/hunt/http/HttpRequest.d
+++ source/hunt/http/HttpRequest.d
@@ -20,13 +20,18 @@ import std.ascii;
 import std.format;
 import std.range;
 
+
+version(WITH_HUNT_TRACE) {
+    import hunt.trace.Tracer;
+}
+
 /**
  * 
  */
 class HttpRequest : HttpMetaData {
     private string _method;
     private HttpURI _uri;
-    private Object attachment;  
+    private Object[string] _attributes;
 
     this(HttpFields fields) {
         this("", null, HttpVersion.Null, fields);
@@ -119,17 +124,109 @@ class HttpRequest : HttpMetaData {
                 || (getHttpVersion() == HttpVersion.HTTP_2 && getContentLength() < 0);
     }
 
+    deprecated("Using getAttribute instead.")
     Object getAttachment() {
-        return attachment;
+        return getAttribute("_attachment");
     }
 
+    deprecated("Using setAttribute instead.")
     void setAttachment(Object attachment) {
-        this.attachment = attachment;
+        // this.attachment = attachment;
+        setAttribute("_attachment", attachment);
+    }
+
+    /**
+     * Returns the value of the user-defined attribute of this connection.
+     *
+     * @param key the key of the attribute
+     * @return <tt>null</tt> if there is no attribute with the specified key
+     */
+    Object getAttribute(string key) {
+        return getAttribute(key, null);
+    }
+
+    /**
+     * Returns the value of user defined attribute associated with the
+     * specified key.  If there's no such attribute, the specified default
+     * value is associated with the specified key, and the default value is
+     * returned.  This method is same with the following code except that the
+     * operation is performed atomically.
+     * <pre>
+     * if (containsAttribute(key)) {
+     *     return getAttribute(key);
+     * } else {
+     *     setAttribute(key, defaultValue);
+     *     return defaultValue;
+     * }
+     * </pre>
+     * 
+     * @param key the key of the attribute we want to retreive
+     * @param defaultValue the default value of the attribute
+     * @return The retrieved attribute or <tt>null</tt> if not found
+     */
+    Object getAttribute(string key, Object defaultValue) {
+        return _attributes.get(key, defaultValue);
+    }
+
+    /**
+     * Sets a user-defined attribute.
+     *
+     * @param key the key of the attribute
+     * @param value the value of the attribute
+     * @return The old value of the attribute.  <tt>null</tt> if it is new.
+     */
+    Object setAttribute(string key, Object value) {
+        auto itemPtr = key in _attributes;
+		Object oldValue = null;
+        if(itemPtr !is null) {
+            oldValue = *itemPtr;
+        }
+        _attributes[key] = value;
+		return oldValue;
+    }
+
+    /**
+     * Removes a user-defined attribute with the specified key.
+     *
+     * @param key The key of the attribute we want to remove
+     * @return The old value of the attribute.  <tt>null</tt> if not found.
+     */
+    Object removeAttribute(string key) {
+        auto itemPtr = key in _attributes;
+        if(itemPtr is null) {
+            return null;
+        } else {
+            Object oldValue = *itemPtr;
+            _attributes.remove(key);
+            return oldValue;
+        }
+    }
+
+    /**
+     * @param key The key of the attribute we are looking for in the connection 
+     * @return <tt>true</tt> if this connection contains the attribute with
+     * the specified <tt>key</tt>.
+     */
+    bool containsAttribute(string key) {
+        auto itemPtr = key in _attributes;
+        return itemPtr !is null;
+    }
+
+    /**
+     * @return the set of keys of all user-defined attributes.
+     */
+    string[] getAttributeKeys() {
+        return _attributes.keys();
     }
 
     override string toString() {
         HttpFields fields = getFields();
         return format("%s{u=%s,%s,h=%d,cl=%d}",
                 getMethod(), getURI(), getHttpVersion(), fields is null ? -1 : fields.size(), getContentLength());
-    }    
+    }
+
+version(WITH_HUNT_TRACE) {
+    Tracer tracer;
+}
+
 }
